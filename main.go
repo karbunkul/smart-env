9a94b3843e4a8377ab97bc3cdaaa23097573d106
karbunkul <karbunkul@yourtask.ru>
2019-03-31 09:55:21 +0500

    load config from yaml

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"github.com/urfave/cli"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
 	"log"
 	"os"
+	"path"
 	"path/filepath"
+	"strings"
 )
 
 func main() {
@@ -49,7 +54,15 @@ func initApp() *cli.App {
 
 // ищем путь к файлу конфигурации
 func findConfFile(dir string, name string) (string, error) {
+	formats := [3]string{"yaml", "yml", "json"}
 
+	for _, format := range formats {
+		file := path.Join(dir, name+"."+format)
+		if _, err := os.Stat(file); !os.IsNotExist(err) {
+			return file, nil
+		}
+	}
+	os.Exit(-1)
 	return "", nil
 }
 
@@ -60,8 +73,13 @@ func getConfigDir(param string) (string, error) {
 		param, _ = os.Getwd()
 	}
 	// полный путь из относительного
-	if path, _ := filepath.Abs(param); filepath.IsAbs(filepath.Dir(path)) == true {
-		param = filepath.Dir(path)
+	if absPath, _ := filepath.Abs(param); filepath.IsAbs(filepath.Dir(absPath)) == true {
+		fi, _ := os.Stat(absPath)
+		if fi.Mode().IsDir() {
+			param = absPath
+		} else {
+			param = path.Dir(absPath)
+		}
 	}
 	// проверяем существует ли директория
 	if _, err := os.Stat(param); !os.IsNotExist(err) {
@@ -81,7 +99,35 @@ func cliMainAction(c *cli.Context) error {
 	configDir, _ := getConfigDir(c.String("config-dir"))
 	configName, _ := getConfigName(c.String("config-name"))
 
-	println(configDir, configName)
+	configPath, _ := findConfFile(configDir, configName)
+
+	data, _ := loadConfig(configPath)
+	fmt.Println(data.Variables)
 
 	return nil
+}
+
+type Config struct {
+	Version   string
+	Variables []struct {
+		Name   string
+		Schema interface{}
+	}
+	Stages interface{}
+}
+
+func loadConfig(path string) (Config, error) {
+	data, _ := ioutil.ReadFile(path)
+	var config Config
+	ext := filepath.Ext(path)[1:]
+
+	switch strings.ToLower(ext) {
+	case "yaml", "yml":
+		err := yaml.Unmarshal(data, &config)
+		if err != nil {
+			log.Fatalf("error: %v", err)
+		}
+		break
+	}
+	return config, nil
 }
